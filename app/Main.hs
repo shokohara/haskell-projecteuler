a6f600fe84a5032afa965a0bbcdf667330fa94f2
shokohara <shokohara@users.noreply.github.com>
2016-12-06 20:12:47 +0900

    Add Problem2

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,14 @@ module Main where
 import Data.Map (Map)
 import qualified Data.Map as Map
 import qualified Projecteuler.Problem01 as P01
+import qualified Projecteuler.Problem02 as P02
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
 
 solutions :: Map Integer Integer
 solutions = Map.fromList [
                            (1, P01.resolve)
+                         , (2, P02.resolve)
                          ]
 
 solution :: Integer -> Maybe Integer
