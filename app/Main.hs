c727aa39efb7cafda116433deb7754c0ac9b0c45
shokohara <shokohara@users.noreply.github.com>
2016-12-06 17:57:32 +0900

    Add Problem1

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,27 @@
 module Main where
 
-import Lib
+import Data.Map (Map)
+import qualified Data.Map as Map
+import qualified Projecteuler.Problem01 as P01
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
+
+solutions :: Map Integer Integer
+solutions = Map.fromList [
+                           (1, P01.resolve)
+                         ]
+
+solution :: Integer -> Maybe Integer
+solution = flip Map.lookup solutions
 
 main :: IO ()
-main = someFunc
+main = do
+  args <- getArgs
+  case args of
+    [number] -> case solution (read number :: Integer) of
+      Just v -> print v
+      Nothing -> putStrLn "There is no solution yet for this problem"
+    _ -> usage >> exitFailure
+  where
+    usage = putStrLn "Usage: stack runghc app/Main.hs [number]"
+
